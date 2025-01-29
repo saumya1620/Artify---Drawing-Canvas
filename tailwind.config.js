5dad235cb03b5d7862cfd1b9b0faf73d9df05ada
aakanksha80 <saumyajain598@gmail.com>
2025-01-29 14:33:09 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./project/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width:
+      {
+        '400':'400px',
+      }
+    },
+  },
+  plugins: [],
+}
+
